9592fc32ff60ca5b2eb4cd7734f3792a6e5b82d8
resteve <resteve@zikzakmedia.com>
2015-12-01 11:03:28 +0100

    Tests

--- tests/test_babi_reports_sale.py
+++ tests/test_babi_reports_sale.py
@@ -1,28 +1,18 @@
-#!/usr/bin/env python
+# This file is part of the babi_reports_sale module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class TestCase(unittest.TestCase):
-    '''
-    Test module.
-    '''
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module(
-            'babi_reports_sale')
-
-    def test0006depends(self):
-        '''
-        Test depends.
-        '''
-        test_depends()
+class BabiReportsSaleTestCase(ModuleTestCase):
+    'Test Babi Reports Sale module'
+    module = 'babi_reports_sale'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
+        BabiReportsSaleTestCase))
     return suite
