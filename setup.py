f6e9a576fada07882aad7bf34f7221666fc8c1a6
Mathias Behrle <mathiasb@m9s.biz>
2024-02-11 12:48:58 +0100

    Use our template for a working setup.py.

--- setup.py
+++ setup.py
@@ -1,38 +1,35 @@
-#!/usr/bin/env python
-# This file is part babi_reports_sale module for Tryton.
+#!/usr/bin/env python3
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 
-from setuptools import setup
-import re
-import os
 import io
+import os
+import re
 from configparser import ConfigParser
 
-MODULE = 'babi_reports_sale'
-PREFIX = 'trytonzz'
-MODULE2PREFIX = {
-    'babi': 'nantic',
-    }
+from setuptools import find_packages, setup
+
+MODULE2PREFIX = {}
 
 
 def read(fname):
-    return io.open(
+    content = io.open(
         os.path.join(os.path.dirname(__file__), fname),
         'r', encoding='utf-8').read()
+    content = re.sub(
+        r'(?m)^\.\. toctree::\r?\n((^$|^\s.*$)\r?\n)*', '', content)
+    return content
 
 
 def get_require_version(name):
-    if minor_version % 2:
-        require = '%s >= %s.%s.dev0, < %s.%s'
-    else:
-        require = '%s >= %s.%s, < %s.%s'
+    require = '%s >= %s.%s, < %s.%s'
     require %= (name, major_version, minor_version,
         major_version, minor_version + 1)
     return require
 
+
 config = ConfigParser()
-config.readfp(open('tryton.cfg'))
+config.read_file(open(os.path.join(os.path.dirname(__file__), 'tryton.cfg')))
 info = dict(config.items('tryton'))
 for key in ('depends', 'extras_depend', 'xml'):
     if key in info:
@@ -41,50 +38,41 @@ version = info.get('version', '0.0.1')
 major_version, minor_version, _ = version.split('.', 2)
 major_version = int(major_version)
 minor_version = int(minor_version)
-
+name = 'm9s_babi_reports_sale'
+download_url = 'https://gitlab.com/m9s/babi_reports_sale.git'
 requires = []
 for dep in info.get('depends', []):
     if not re.match(r'(ir|res)(\W|$)', dep):
         prefix = MODULE2PREFIX.get(dep, 'trytond')
         requires.append(get_require_version('%s_%s' % (prefix, dep)))
-requires.append(get_require_version('trytond'))
+requires.append(get_require_version('m9s-trytond'))
 
 tests_require = []
-series = '%s.%s' % (major_version, minor_version)
-if minor_version % 2:
-    branch = 'default'
-else:
-    branch = series
-dependency_links = [
-    ('hg+https://bitbucket.org/nantic/'
-        'trytond-babi@%(branch)s'
-        '#egg=nantic-babi-%(series)s' % {
-            'branch': branch,
-            'series': series,
-            }),
-    ]
-if minor_version % 2:
-    # Add development index for testing with proteus
-    dependency_links.append('https://trydevpi.tryton.org/')
 
-setup(name='%s_%s' % (PREFIX, MODULE),
+setup(name=name,
     version=version,
     description='Tryton Babi Reports Sale Module',
-    long_description=read('README'),
-    author='Zikzakmedia SL',
-    author_email='zikzak@zikzakmedia.com',
-    url='https://bitbucket.org/zikzakmedia/',
-    download_url='https://bitbucket.org/zikzakmedia/trytond-%s' % MODULE,
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    author='MBSolutions',
+    author_email='info@m9s.biz',
+    url='https://www.m9s.biz/',
+    download_url=download_url,
+    project_urls={
+        "Bug Tracker": 'https://support.m9s.biz/',
+        "Source Code": 'https://gitlab.com/m9s/babi_reports_sale.git',
+        },
     keywords='',
-    package_dir={'trytond.modules.%s' % MODULE: '.'},
-    packages=[
-        'trytond.modules.%s' % MODULE,
-        'trytond.modules.%s.tests' % MODULE,
-        ],
+    package_dir={'trytond.modules.babi_reports_sale': '.'},
+    packages=(
+        ['trytond.modules.babi_reports_sale']
+        + ['trytond.modules.babi_reports_sale.%s' % p
+            for p in find_packages()]
+        ),
     package_data={
-        'trytond.modules.%s' % MODULE: (info.get('xml', [])
-            + ['tryton.cfg', 'view/*.xml', 'locale/*.po', '*.odt',
-                'icons/*.svg', 'tests/*.rst']),
+        'trytond.modules.babi_reports_sale': (info.get('xml', [])
+            + ['tryton.cfg', 'view/*.xml', 'locale/*.po', '*.fodt',
+                'icons/*.svg', 'tests/*.rst', 'tests/*.json']),
         },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -93,39 +81,48 @@ setup(name='%s_%s' % (PREFIX, MODULE),
         'Intended Audience :: Developers',
         'Intended Audience :: Financial and Insurance Industry',
         'Intended Audience :: Legal Industry',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'License :: OSI Approved :: '
+        'GNU General Public License v3 or later (GPLv3+)',
         'Natural Language :: Bulgarian',
         'Natural Language :: Catalan',
+        'Natural Language :: Chinese (Simplified)',
         'Natural Language :: Czech',
         'Natural Language :: Dutch',
         'Natural Language :: English',
+        'Natural Language :: Finnish',
         'Natural Language :: French',
         'Natural Language :: German',
         'Natural Language :: Hungarian',
+        'Natural Language :: Indonesian',
         'Natural Language :: Italian',
+        'Natural Language :: Persian',
+        'Natural Language :: Polish',
         'Natural Language :: Portuguese (Brazilian)',
+        'Natural Language :: Romanian',
         'Natural Language :: Russian',
         'Natural Language :: Slovenian',
         'Natural Language :: Spanish',
+        'Natural Language :: Turkish',
+        'Natural Language :: Ukrainian',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Office/Business',
         ],
     license='GPL-3',
+    python_requires='>=3.8',
     install_requires=requires,
-    dependency_links=dependency_links,
+    extras_require={
+        'test': tests_require,
+        },
     zip_safe=False,
     entry_points="""
     [trytond.modules]
-    %s = trytond.modules.%s
-    """ % (MODULE, MODULE),
-    test_suite='tests',
-    test_loader='trytond.test_loader:Loader',
-    tests_require=tests_require,
-    use_2to3=True,
+    babi_reports_sale = trytond.modules.babi_reports_sale
+    """,  # noqa: E501
     )
--- tests/test_module.py
+++ tests/test_module.py
@@ -1,13 +1,11 @@
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 
-# This file is part of Tryton.  The COPYRIGHT file at the top level of
-# this repository contains the full copyright notices and license terms.
-
-from trytond.modules.company.tests import CompanyTestMixin
 from trytond.tests.test_tryton import ModuleTestCase
 
 
-class BabiReportsSaleTestCase(CompanyTestMixin, ModuleTestCase):
-    'Test BabiReportsSale module'
+class BabiReportsSaleTestCase(ModuleTestCase):
+    "Test Babi Reports Sale module"
     module = 'babi_reports_sale'
 
 
